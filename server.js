85f58e39d3d874b20da231b13051f7bd41dfadf9
Cristian Morales <cmoralesl@psl.com.co>
2020-09-11 18:34:30 -0500

    change year movie

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/movies', function(req, res){
   var movies = [
     {title : 'Suicide Squad', release: '2020', score: 8, reviewer: 'Robert Smith', publication : 'The Daily Reviewer'},    
     {title : 'Batman vs. Superman', release : '2116', score: 6, reviewer: 'Chris Harris', publication : 'International Movie Critic'},
-    {title : 'Captain America: Civil War', release: '2016', score: 9, reviewer: 'Janet Garcia', publication : 'MoviesNow'},
+    {title : 'Captain America: Civil War', release: '2020', score: 9, reviewer: 'Janet Garcia', publication : 'MoviesNow'},
     {title : 'Deadpool', release: '2016', score: 9, reviewer: 'Andrew West', publication : 'MyNextReview'},
     {title : 'Avengers: Age of Ultron', release : '2015', score: 7, reviewer: 'Mindy Lee', publication: 'Movies n\' Games'},
     {title : 'Ant-Man', release: '2015', score: 8, reviewer: 'Martin Thomas', publication : 'TheOne'},

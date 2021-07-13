4fe04dfe1a63c10dd8117de33594c08383cbe632
Andres Lopez Bedoya <andres.lopezb@perficient.com>
2021-07-13 10:46:39 -0500

    Stablish connection with db

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // Get our dependencies
 var express = require('express');
 var app = express();
-//var mysql = require("mysql");
-//var connection = mysql.createConnection({
-//  host     : process.env.DB_HOST || 'mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com',
-//  user     : process.env.DB_USER || 'applicationuser',
-//  password : process.env.DB_PASS || 'applicationuser',
-//  database : process.env.DB_NAME || 'movie_db'
-//});
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST || 'mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com',
+  user     : process.env.DB_USER || 'applicationuser',
+  password : process.env.DB_PASS || 'applicationuser',
+  database : process.env.DB_NAME || 'movie_db'
+});
 
-//connection.connect();
+connection.connect();
 
 //function getMovies(callback) {    
 //        connection.query("SELECT * FROM movie_db.movies",
